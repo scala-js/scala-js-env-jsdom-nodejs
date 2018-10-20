770e0f6b33787b40c7baf8ae404d2a6e2b8d5f87
Tobias Schlatter <tobias@meisch.ch>
2018-10-20 08:54:40 +0200

    Upgrade to Scala.js 1.0.0-M6

--- jsdom-nodejs-env/src/test/scala/org/scalajs/jsenv/jsdomnodejs/JSDOMNodeJSEnvTest.scala
+++ jsdom-nodejs-env/src/test/scala/org/scalajs/jsenv/jsdomnodejs/JSDOMNodeJSEnvTest.scala
@@ -1,42 +1,24 @@
 package org.scalajs.jsenv.jsdomnodejs
 
-import scala.concurrent.Await
+import scala.concurrent.duration._
 
 import org.junit.Test
 
-import org.scalajs.io._
-
-import org.scalajs.jsenv._
+import org.scalajs.jsenv.test.kit.TestKit
 
 class JSDOMNodeJSEnvTest {
-
-  private val TestRunConfig = {
-    RunConfig()
-      .withInheritOut(false)
-      .withOnOutputStream((_, _) => ()) // ignore stdout
-  }
-
-  private val config = JSDOMNodeJSSuite.Config
+  private val kit = new TestKit(new JSDOMNodeJSEnv, 1.minute)
 
   @Test
-  def historyAPIWithoutTestKit: Unit = {
-    assertRunSucceeds(
+  def historyAPI: Unit = {
+    kit.withRun(
         """
         |console.log(window.location.href);
         |window.history.pushState({}, "", "/foo");
         |console.log(window.location.href);
-        """.stripMargin)
-  }
-
-  private def assertRunSucceeds(inputStr: String): Unit = {
-    val inputFile = MemVirtualBinaryFile.fromStringUTF8("test.js", inputStr)
-    val input = Input.ScriptsToLoad(List(inputFile))
-    val run = config.jsEnv.start(input, TestRunConfig)
-    try {
-      Await.result(run.future, config.awaitTimeout)
-    } finally {
-      run.close()
+        """.stripMargin) {
+      _.expectOut("http://localhost/\n")
+        .expectOut("http://localhost/foo\n")
     }
   }
-
 }
--- project/plugins.sbt
+++ project/plugins.sbt
@@ -1,6 +1,6 @@
-addSbtPlugin("org.scala-js" % "sbt-scalajs" % "1.0.0-M5")
+addSbtPlugin("org.scala-js" % "sbt-scalajs" % "1.0.0-M6")
 
-libraryDependencies += "org.scala-js" %% "scalajs-env-nodejs" % "1.0.0-M5"
+libraryDependencies += "org.scala-js" %% "scalajs-env-nodejs" % "1.0.0-M6"
 
 unmanagedSourceDirectories in Compile +=
   baseDirectory.value.getParentFile / "jsdom-nodejs-env/src/main/scala"
